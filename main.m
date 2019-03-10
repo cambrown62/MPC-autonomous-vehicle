6f6d1fd17d3ae65cc3ea7a7dd98bd11706dc992f
Cameron Brown <cambrown62@yahoo.com>
2019-03-10 19:28:18 -0400

    assume constant velocity

--- main.m
+++ main.m
@@ -27,11 +27,11 @@ a3 = .01;
 x_kp1 = @(x, v, psi) x + v*cos(psi)*h;
 y_kp1 = @(y, v, psi) y + v*sin(psi)*h;
 psi_kp1 = @(psi, v, del) psi + (v/l_f)*del*h;
-v_kp1 = @(v, acc) v + acc*h;
+v_kp1 = @(v) v; % assume constant velocity
 
 % heading error and cross track error
-heading_err = @(psi, desired_psi, v, del) psi + desired_psi * (v/l_f) * del*h;
-crosstrack_err = @(path_y, y, v, psi_err) path_y - y + v*sin(psi_err)*h;
+heading_err = @(desired_psi, psi) desired_psi - psi;
+crosstrack_err = @(path_y, y) path_y - y;
 
 %% Cost Function
 
@@ -44,6 +44,7 @@ w_a = 1;
 w_del_dot = 1;
 w_jerk = 1;
 
+J = @(del)
 
 % coords for entire path (we are given this by motion planner)
 path_x = [0:.1:10];
@@ -53,19 +54,19 @@ desired_psi = atan(path_x/sqrt(100-path_x.^2)); % heading of the path at every p
 % initial vehicle states
 x0 = -0.01;
 y0 = -0.01;
-v0 = 0;
+v0 = 1;
 acc0 = 0;
 psi0 = 0;
-del0 = 0;
-psi_err0 = heading_err(psi0, desired_psi(1), v0, del0);
-cte0 = crosstrack_err(path_y(1), y0, v0, psi_err0);
+%%del0 = 0;
+psi_err0 = heading_err(desired_psi(1), psi0);
+cte0 = crosstrack_err(path_y(1), y0);
 
 x_out = [x0 []];
 y_out = [y0 []];
-v_out = [v0 []];
+%v_out = [v0 []];
 acc_out = [acc0 []];
 psi_out = [psi0 []];
-del_out = [del0 []];
+del_out = [];
 psi_err = [psi_err0 []];
 cte_out = [cte0 []];
 
@@ -85,17 +86,20 @@ for t = 1:(length(path_x)-6)
     x_pred(1) = x_out(t);
     y_pred(1) = y_out(t);
     psi_pred(1) = psi_out(t);
-    v_pred(1) = v_out(t);
+    %v_pred(1) = v_out(t);
     cte_pred(1) = cte_out(t);
     psi_err_pred(1) = psi_err_out(t);
     
     for i = 2:N
-        x_pred(i) = x_kp1(x_pred(i-1), v_pred(i-1), psi_pred(i-1));
-        y_pred(i) = y_kp1(y_pred(i-1), v_pred(i-1), psi_pred(i-1));
-        psi_pred(i) = psi_kp1(psi_pred(i-1), v_pred(i-1), del);
-        v_pred(i) = v_kp1(v_pred(i-1), acc);
-        psi_err(i) = heading_err(psi_pred(i), desired_psi(), v_pred(i), del);
-        cte_pred(i) = crosstrack_error(path_y(),y_pred(i), v_pred(i), psi_err(i));
+        x_pred(i) = x_kp1(x_pred(i-1), v0, psi_pred(i-1));
+        y_pred(i) = y_kp1(y_pred(i-1), v0, psi_pred(i-1));
+        psi_pred(i) = psi_kp1(psi_pred(i-1), v0, del);
+        %v_pred(i) = v_kp1(v_pred(i-1), acc);
+        psi_err(i) = heading_err( desired_psi(), psi_pred(i));
+        cte_pred(i) = crosstrack_error(path_y(), y_pred(i));
+        
+        
+        
     end
     
 end
